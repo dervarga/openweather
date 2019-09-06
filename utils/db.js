48027742080fc6b4a1130f6c81eb4826efce19cf
dantschi <dantschi@gmail.com>
2019-09-06 14:33:45 +0200

    db is now commented out

--- utils/db.js
+++ utils/db.js
@@ -1,34 +1,34 @@
-////////////////////////////////////////
-// DATABASE QUERIES
-////////////////////////////////////////
-
-const spicedPg = require("spiced-pg");
-// let username, pwd;
-try {
-  var { username, pwd } = require("./secrets.json");
-} catch (err) {
-  console.log(err);
-}
-
-////////////////////////////////////////
-// SETTINGS FOR PSQL CONNECTION
-////////////////////////////////////////
-const dbUrl =
-  process.env.DATABASE_URL ||
-  `postgres:${username}:${pwd}@localhost:5432/openweather_cities`;
-const db = spicedPg(dbUrl);
-
-////////////////////////////////////////
-// GETTING THE CITY NAMES
-////////////////////////////////////////
-
-module.exports.getCity = function getCity(name) {
-  console.log(name);
-  return db.query(
-    `
-    SELECT * FROM cities WHERE LOWER(name) LIKE LOWER($1 || '%')
-    LIMIT 50;
-    `,
-    [name]
-  );
-};
+// ////////////////////////////////////////
+// // DATABASE QUERIES
+// ////////////////////////////////////////
+//
+// const spicedPg = require("spiced-pg");
+// // let username, pwd;
+// try {
+//   var { username, pwd } = require("./secrets.json");
+// } catch (err) {
+//   console.log(err);
+// }
+//
+// ////////////////////////////////////////
+// // SETTINGS FOR PSQL CONNECTION
+// ////////////////////////////////////////
+// const dbUrl =
+//   process.env.DATABASE_URL ||
+//   `postgres:${username}:${pwd}@localhost:5432/openweather_cities`;
+// const db = spicedPg(dbUrl);
+//
+// ////////////////////////////////////////
+// // GETTING THE CITY NAMES
+// ////////////////////////////////////////
+//
+// module.exports.getCity = function getCity(name) {
+//   console.log(name);
+//   return db.query(
+//     `
+//     SELECT * FROM cities WHERE LOWER(name) LIKE LOWER($1 || '%')
+//     LIMIT 50;
+//     `,
+//     [name]
+//   );
+// };
