8a3b62dfe1b71b60691704533420f0683272ff9c
dantschi <dantschi@gmail.com>
2019-09-06 14:31:29 +0200

    try bugfix with node-heroku

--- utils/searchCity.js
+++ utils/searchCity.js
@@ -1,13 +1,13 @@
-const elasticlunr = require('elasticlunr');
-const cities = require("../../city.list.json");
-
-const index = elasticlunr(function () {
-    this.addField('name');
-});
-index.addDoc(cities);
-
-module.exports.searchCity = function searchCity(){
-  let rslt = index.search("Vac");
-  console.log(rslt);
-  return rslt;
-}
+// const elasticlunr = require('elasticlunr');
+// const cities = require("../../city.list.json");
+//
+// const index = elasticlunr(function () {
+//     this.addField('name');
+// });
+// index.addDoc(cities);
+//
+// module.exports.searchCity = function searchCity(){
+//   let rslt = index.search("Vac");
+//   console.log(rslt);
+//   return rslt;
+// }
